1f14d9192b951ca37516450f85dd12d91540a477
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-10-22 02:01:29 -0400

    Setup

---
+++ main.py
@@ -0,0 +1,21 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+
+turtle.shape(image)
+
+answer_state = screen.textinput(title = "Guess the State", prompt="What's another state name")
+
+
+# The code below is how we got the x and y values of all states in the csv file
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+#
+#
+# turtle.onscreenclick(get_mouse_click_coor)
+#
+# turtle.mainloop()
+
